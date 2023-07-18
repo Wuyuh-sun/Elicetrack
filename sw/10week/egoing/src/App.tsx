0e6ead88b2647f7a92007204003849ae1238dc1f
wooyunha <wjh0970@gmail.com>
2023-07-18 23:58:33 +0900

    10주차 실습

--- sw/10week/egoing/src/App.tsx
+++ sw/10week/egoing/src/App.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+type counterProps = {
+  title: string,
+  initValue: number
+}
+
+// @ts-ignore
+function Counter({ title, initValue }: counterProps) {
+  let countState = useState(initValue);
+  let count = countState[0];
+  let setCount = countState[1];
+  return (
+    <>
+      <h1>{title}</h1>
+      <button onClick={() => { setCount(count + 1) }}>+</button> {count}
+    </>
+  )
+}
 
 function App() {
   return (
     <div>
-      <h1>Counter</h1>
-      <button>+</button> 0
+      <Counter title="카운터" initValue={0} />
+      <Counter title="Counter" initValue={10} />
+      <Counter title="불면증 카운터" initValue={20} />
+      <Counter title={"undefined"} initValue={0} />
     </div>
   );
 }
