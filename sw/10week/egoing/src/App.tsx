23670ba8350adf51e8da2199c0116e03ab6fa1d7
wooyunha <wjh0970@gmail.com>
2023-07-21 00:46:19 +0900

    react hook 실습

--- sw/10week/egoing/src/App.tsx
+++ sw/10week/egoing/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button, Container, Grid } from '@mui/material';
 import React, { useState } from 'react';
 type counterProps = {
   title: string,
@@ -6,25 +7,40 @@ type counterProps = {
 
 // @ts-ignore
 function Counter({ title, initValue }: counterProps) {
-  let countState = useState(initValue);
-  let count = countState[0];
-  let setCount = countState[1];
+  // let countState = useState(initValue);
+  // let count = countState[0];
+  // let setCount = countState[1];
+  const [count, setCount] = useState(initValue);
   return (
-    <>
+    <div style={{
+      border: "10px solid black",
+      fontSize: "20px",
+      padding: 10
+    }}>
       <h1>{title}</h1>
-      <button onClick={() => { setCount(count + 1) }}>+</button> {count}
-    </>
+      <Button variant='contained' onClick={() => { setCount(count + 1) }}>+</Button> {count}
+    </div>
   )
 }
 
 function App() {
   return (
-    <div>
-      <Counter title="카운터" initValue={0} />
-      <Counter title="Counter" initValue={10} />
-      <Counter title="불면증 카운터" initValue={20} />
-      <Counter title={"undefined"} initValue={0} />
-    </div>
+    <Container maxWidth="md">
+      <Grid container spacing={1}>
+        <Grid item xs={24} sm={6} md={4}>
+          <Counter title="카운터" initValue={0} />
+        </Grid>
+        <Grid item xs={24} sm={6} md={4}>
+          <Counter title="Counter" initValue={10} />
+        </Grid>
+        <Grid item xs={24} sm={6} md={4}>
+          <Counter title="불면증 카운터" initValue={20} />
+        </Grid>
+        <Grid item xs={24} sm={6} md={4}>
+          <Counter title={"undefined"} initValue={0} />
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
